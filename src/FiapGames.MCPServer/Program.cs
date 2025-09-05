42b16256e375db7edd143f84f6556bcc9c6d925e
Igor Daflon <igordafloncouto@gmail.com>
2025-09-05 08:02:10 -0300

    Cria tools jogos

--- src/FiapGames.MCPServer/Program.cs
+++ src/FiapGames.MCPServer/Program.cs
@@ -1,1 +1,38 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using FiapGames.MCPServer.Clients;
+using FiapGames.MCPServer.Tools;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using ModelContextProtocol.Protocol;
+
+var builder = Host.CreateApplicationBuilder(args);
+
+builder.Logging.ClearProviders();
+builder.Logging.AddConsole(options =>
+{
+    options.LogToStandardErrorThreshold = LogLevel.Debug;
+});
+
+builder.Configuration.AddEnvironmentVariables();
+
+var serverInfo = new Implementation { Name = "DotnetMCPServer.Stdio", Version = "1.0.0" };
+builder.Services
+    .AddMcpServer(mcp =>
+    {
+        mcp.ServerInfo = serverInfo;
+    })
+    .WithStdioServerTransport()
+    .WithToolsFromAssembly(typeof(FiapGamesTools).Assembly);
+
+builder.Services.AddHttpClient<JogoApiClient>(client =>
+{
+    var baseAddress = Environment.GetEnvironmentVariable("API_BASE_ADDRESS");
+    if (!string.IsNullOrEmpty(baseAddress))
+        client.BaseAddress = new Uri(baseAddress);
+    else
+        client.BaseAddress = new Uri("https://localhost:7091/api/");
+});
+
+var app = builder.Build();
+await app.RunAsync();
